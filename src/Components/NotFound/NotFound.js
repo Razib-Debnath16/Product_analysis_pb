cf4d0069c522a3970eafb6275b878f58a3b1ebe6
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-04-04 21:18:51 +0600

    adding not found

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import img from '../../Image/404.jpeg';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>kisui pailam na</h2>
+        <div className='notFound'>
+            <img src={img} alt="" />
         </div>
     );
 };
--- src/Components/PieCharts/PieCharts.js
+++ src/Components/PieCharts/PieCharts.js
@@ -5,7 +5,7 @@ const PieCharts = (props) => {
     return (
         <div>
             <h3>Investment vs Revenue</h3>
-            <PieChart width={400} height={250}>
+            <PieChart width={600} height={250}>
                 <Pie data={props.products} dataKey="investment" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8"></Pie>
                 <Pie data={props.products} dataKey="revenue" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d"></Pie>
             </PieChart>
